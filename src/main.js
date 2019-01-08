c5c08bd700fb64612be82cd0742769d60c72c42b
weibo <745404604@qq.com>
2019-01-08 19:03:36 +0800

    iview

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import App from './App'
 import router from './router'
 import vueResource from 'vue-resource'
 import './common/css/reset.css'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
 Vue.use(vueResource)
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
